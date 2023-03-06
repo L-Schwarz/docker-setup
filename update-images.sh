90d03c59186f28078c99761b191d4f3156a3009c
Luca Schwarz <lucky.f-black@web.de>
2023-03-06 22:06:38 +0000

    :sparkles: add shell script to update images

---
+++ update-images.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+docker pull nextcloud
+docker pull jwilder/nginx-proxy
+docker pull jrcs/letsencrypt-nginx-proxy-companion
+docker pull gitea/gitea
+docker pull oznu/cloudflare-ddns
+docker pull portainer/portainer-ce
+docker pull vaultwarden/server
