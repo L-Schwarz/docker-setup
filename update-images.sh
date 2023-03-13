fb0af60e6b32108fe0f9021811ec450e98c5178c
Luca Schwarz <lucky.f-black@web.de>
2023-03-13 12:28:18 +0000

    :arrow_up: update letsencrypt image to a newer one

--- update-images.sh
+++ update-images.sh
@@ -2,7 +2,7 @@
 
 docker pull nextcloud
 docker pull jwilder/nginx-proxy
-docker pull jrcs/letsencrypt-nginx-proxy-companion
+docker pull nginxproxy/acme-companion
 docker pull gitea/gitea
 docker pull oznu/cloudflare-ddns
 docker pull portainer/portainer-ce
